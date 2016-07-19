3ab78b520c80f66ad54636198195bf476c29d4f6
tusharywaghulde <tusharywaghulde9@gmail.com>
2016-07-19 08:35:52 +0530

    Add files via upload

---
+++ meta.rb
@@ -0,0 +1,5 @@
+class Test
+puts "the definition of class Test"
+puts "self = #{self}"
+puts "Class of self = #{self.class}"
+end
---
+++ meta1.rb
@@ -0,0 +1,12 @@
+class Test
+def one
+ @var =10
+ two
+ end
+ def two
+ 	@var=20
+ puts @var
+ end
+ end
+ t=Test.new
+ t.one
